4447ac805c0d61b30653a8331dc541a6be47c6fd
chrisK00 <christian.khalil2000@gmail.com>
2022-02-21 19:22:20 +0100

    production url

--- MemorySignal/Client/Program.cs
+++ MemorySignal/Client/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+string baseHttpClientUrl = builder.HostEnvironment.IsProduction() ? builder.Configuration["ApiUrl"]  : builder.HostEnvironment.BaseAddress;
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseHttpClientUrl) });
 
 await builder.Build().RunAsync();
