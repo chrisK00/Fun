7194769d0f2e635956704d7c4431402bd5996e47
chrisK00 <christian.khalil2000@gmail.com>
2022-02-22 10:40:23 +0100

    blazor form

---
+++ MemorySignal/MemorySignal.Admin/Models/AddCardCollectionModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemorySignal.Admin.Models;
+public class AddCardCollectionModel
+{
+    [Required, MinLength(2)] public string Name { get; set; }
+    [Required] public string FolderPath { get; set; }
+}
--- MemorySignal/Shared/Requests/AddCardCollectionRequest.cs
+++ MemorySignal/Shared/Requests/AddCardCollectionRequest.cs
@@ -5,5 +5,5 @@ namespace MemorySignal.Shared.Requests;
 public class AddCardCollectionRequest
 {
     [Required, MinLength(2)] public string Name { get; set; }
-
+    public List<string> FilePaths { get; set; } = new();
 }
