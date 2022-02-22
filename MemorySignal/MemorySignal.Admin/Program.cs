c3324e39766db8b2038dbdeed707133977f7c388
chrisK00 <christian.khalil2000@gmail.com>
2022-02-22 10:20:28 +0100

    appsettings.json inside wwwroot

--- MemorySignal/MemorySignal.Admin/Program.cs
+++ MemorySignal/MemorySignal.Admin/Program.cs
@@ -1,4 +1,5 @@
 using MemorySignal.Admin;
+using MemorySignal.Core.Extensions;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddCoreServices(builder.Configuration, true);
 
 await builder.Build().RunAsync();
---
+++ MemorySignal/Shared/Requests/AddCardCollectionRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemorySignal.Shared.Requests;
+
+public class AddCardCollectionRequest
+{
+    [Required, MinLength(2)] public string Name { get; set; }
+
+}
