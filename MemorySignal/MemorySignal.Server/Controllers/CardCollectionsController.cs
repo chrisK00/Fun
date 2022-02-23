d8a012b36dfb2939cd5277ef27ea996798baaf19
chrisK00 <christian.khalil2000@gmail.com>
2022-02-23 11:32:01 +0100

    server

---
+++ MemorySignal/MemorySignal.Server/Controllers/CardCollectionsController.cs
@@ -0,0 +1,36 @@
+﻿using MemorySignal.Server.Filters;
+using MemorySignal.Shared.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemorySignal.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CardCollectionsController : ControllerBase
+{
+    private readonly ICardCollectionQueries _cardCollectionQueries;
+
+    public CardCollectionsController(ICardCollectionQueries cardCollectionQueries)
+    {
+        _cardCollectionQueries = cardCollectionQueries;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CardCollectionResponse>>> GetAll()
+    {
+        return Ok(await _cardCollectionQueries.GetAll());
+    }
+
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ServiceFilter(typeof(TokenFilter))]
+    [HttpPost]
+    public async Task<ActionResult<CardCollectionResponse>> AddCardCollection([FromForm] AddCardCollectionRequest request,
+        [FromServices] IAction<AddCardCollectionRequest, CardCollectionResponse> action)
+    {
+        var response = await action.Execute(request);
+
+        if (action.HasErrors) return BadRequest(action.ErrorsFormatted);
+
+        return response;
+    }
+}
--- MemorySignal/MemorySignal.Server/Program.cs
+++ MemorySignal/MemorySignal.Server/Program.cs
@@ -1,17 +1,76 @@
+using MemorySignal.Core.Data;
+using MemorySignal.Server.Filters;
+using MemorySignal.Server.Hubs;
+using MemorySignal.Shared.Interfaces;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opt => opt.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" }));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(Log.Logger);
+
+builder.Services.AddScoped<TokenFilter>();
+builder.Services.AddCoreServices(builder.Configuration, builder.Environment.IsProduction());
 
 var app = builder.Build();
+Seed(app.Services);
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
-app.UseAuthorization();
+if (app.Environment.IsProduction())
+{
+    app.UseCors(builder => builder.WithOrigins(app.Configuration["ClientUrl"], app.Configuration["AdminUrl"]).AllowAnyHeader().AllowAnyMethod());
+    app.MapFallback(async req => await req.Response.WriteAsJsonAsync("This link appears to be broken or the site is down for maintenance"));
+}
 
 app.MapControllers();
+app.MapHub<MemoryGameHub>("/" + IMemoryGameHub.Uri);
 
 app.Run();
+
+static void Seed(IServiceProvider sp)
+{
+    using var scope = sp.CreateScope();
+    using var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+    context.Database.Migrate();
+    if (context.CardCollections.Any()) return;
+
+    var smallAnimalCards = new List<Card>()
+    {
+        new Card("https://res.cloudinary.com/dbb9v8ne8/image/upload/v1645455062/Cards/bird-gd8d8e4a91_640_sctbst.jpg", "bird-gd8d8e4a91_640_sctbst"),
+        new Card("https://res.cloudinary.com/dbb9v8ne8/image/upload/v1645455062/Cards/bird-g4e9c2c7b0_640_prkfyy.jpg", "bird-g4e9c2c7b0_640_prkfyy"),
+        new Card("https://res.cloudinary.com/dbb9v8ne8/image/upload/v1645455062/Cards/black-and-white-warbler-gb189f6056_640_hrhl6f.jpg", "black-and-white-warbler-gb189f6056_640_hrhl6f"),
+        new Card("https://res.cloudinary.com/dbb9v8ne8/image/upload/v1645455062/Cards/squirrel-gd5b52f387_640_oixdk0.jpg", "squirrel-gd5b52f387_640_oixdk0"),
+    };
+
+    var bigAnimalCards = new List<Card>()
+    {
+        new Card("https://res.cloudinary.com/dbb9v8ne8/image/upload/v1645455145/Cards/dog-gec88cbf11_640_ww0aib.jpg","dog-gec88cbf11_640_ww0aib"),
+        new Card("https://res.cloudinary.com/dbb9v8ne8/image/upload/v1645455145/Cards/dog-gdf5cd6fc2_640_jgcci2.jpg","dog-gdf5cd6fc2_640_jgcci2"),
+        new Card("https://res.cloudinary.com/dbb9v8ne8/image/upload/v1645455145/Cards/labrador-retriever-g30db09fa2_640_kvcgx0.jpg","labrador-retriever-g30db09fa2_640_kvcgx0"),
+    };
+
+    var smallAnimalsCardCollection = new CardCollection("Small Animals", smallAnimalCards);
+    var bigAnimalsCardCollection = new CardCollection("Big Animals", bigAnimalCards);
+    context.AddRange(smallAnimalsCardCollection, bigAnimalsCardCollection);
+    context.SaveChanges();
+}
