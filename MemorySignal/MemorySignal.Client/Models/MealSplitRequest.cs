f2a21b2e0d33708d1a258145a1a468bb1cf5e699
chrisK00 <christian.khalil2000@gmail.com>
2022-02-24 11:04:34 +0100

    meal split

---
+++ MemorySignal/MemorySignal.Client/Models/MealSplitRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemorySignal.Client.Models;
+public class MealSplitRequest
+{
+    [Required, Range(1, int.MaxValue, ErrorMessage = "Enter a minimum of 1 gram")]
+    public int Gram { get; set; }
+
+    [Required, Range(2, int.MaxValue, ErrorMessage = "Split it in to at least 2 portions")]
+    public int Portions { get; set; }
+}
