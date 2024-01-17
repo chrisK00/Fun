4e95e18fda7bb722efe186deccca2dfe5b388d52
ChrisK00 <christian.khalil2000@gmail.com>
2024-01-17 18:09:17 +0100

    a

--- MemorySignal/MemorySignal.Core/Managers/ImageManager.cs
+++ MemorySignal/MemorySignal.Core/Managers/ImageManager.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Options;
 
 namespace MemorySignal.Core.Managers;
 
+// TODO might need to replace with local opt
 public class ImageManager : IImageManager
 {
     private readonly ImageApiOptions _apiOptions;
