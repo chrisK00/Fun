c11c8a3b2215c070ee2129344c07719ff02132cd
ChrisK00 <christian.khalil2000@gmail.com>
2024-06-14 21:17:41 +0200

    blop

--- MemorySignal/MemorySignal.Client/Program.cs
+++ MemorySignal/MemorySignal.Client/Program.cs
@@ -6,7 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-string apiUrl = builder.HostEnvironment.IsProduction() ? builder.Configuration["ApiUrl"] : builder.Configuration["LocalClient"];
+//string apiUrl = builder.HostEnvironment.IsProduction() ? builder.Configuration["ApiUrl"] : builder.Configuration["LocalClient"]; "LocalClient": "http://localhost:5190/"
+string apiUrl = builder.Configuration["ApiUrl"];
 
 builder.Services.Configure<ApiOptions>(opt => opt.Url = apiUrl);
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(apiUrl) });
